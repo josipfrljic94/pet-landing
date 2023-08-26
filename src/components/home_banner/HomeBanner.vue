fd66ade0cd598102d33ee67c6b0e98deafb94dc4
josipfrljic94 <josipfrljic77@gmail.com>
2023-08-26 18:43:31 +0200

    add landing banner

---
+++ src/components/home_banner/HomeBanner.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+defineProps<{ title?: string }>()
+</script>
+
+<template>
+    <img class="img_landing" src="/home-baner.png" alt="home-landing">
+</template>
+
+<style scoped>
+.img_landing{
+    width:100%;
+    min-height:200px;
+    object-fit:cover;
+}
+</style>
