002888bcd689e8136bdb2245202acdb2ba135526
josipfrljic94 <josipfrljic77@gmail.com>
2023-08-28 20:47:22 +0200

    feat: implement fade in in banner title

--- src/components/home_banner/HomeBanner.vue
+++ src/components/home_banner/HomeBanner.vue
@@ -2,13 +2,24 @@
 
 </script>
 <template>
-<img class="img_landing" src="/home-baner.png" alt="home-landing">
+    <img class="img_landing" src="/home-baner.png" alt="home-landing">
 </template>
 
 <style scoped>
-.img_landing{
-    width:100%;
-    min-height:200px;
-    object-fit:cover;
+.img_landing {
+    width: 100%;
+    min-height: 200px;
+    object-fit: cover;
+    animation: fadeIn 0.7s ease-out;
+}
+
+@keyframes fadeIn {
+    0% {
+        opacity: 0;
+    }
+
+    100% {
+        opacity: 1;
+    }
 }
 </style>
